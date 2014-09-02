c0db302278ab4a6b5f53c5142ee9104b1824dc91
Fuad Saud <fuadksd@gmail.com>
2014-09-02 16:33:20 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'pry', group: :development
+
+gem 'activesupport'
+gem 'poltergeist'
+gem 'capybara'
+gem 'cape-cod'
---
+++ daily_meal.rb
@@ -0,0 +1,68 @@
+require 'capybara'
+require 'capybara/poltergeist'
+require 'active_support/time'
+require 'cape-cod'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, phantomjs_logger: File.open(File::NULL, 'w'))
+end
+
+Capybara.default_driver = :poltergeist
+
+class EServices
+  include Capybara::DSL
+
+  BASE_URL = 'https://www.rit.edu/eservices/'
+  USERNAME = 'ffs3415'
+  PASSWORD = 'hanna-1RIT'
+
+  MEAL_PLAN_END_DATE = Date.new(2014, 12, 18)
+
+  def call
+    auth(USERNAME, PASSWORD)
+
+    visit BASE_URL
+
+    login
+
+    puts CapeCod.green "TOTAL BALANCE: $#{ debit_balance }"
+    puts CapeCod.red   "DAILY BALANCE: $#{ daily_balance }"
+  end
+
+  private
+
+  def daily_balance
+    debit_balance / remaining_days
+  end
+
+  def debit_balance
+    sleep 1
+    find('td', text: /Food/).parent.all('td')[15].text[1..-1].to_f
+  end
+
+  def login
+    click_link 'Login to eServices'
+  end
+
+  def auth(name, password)
+    if page.driver.respond_to?(:basic_auth)
+      page.driver.basic_auth(name, password)
+    elsif page.driver.respond_to?(:basic_authorize)
+      page.driver.basic_authorize(name, password)
+    elsif page.driver.respond_to?(:browser) && page.driver.browser.respond_to?(:basic_authorize)
+      page.driver.browser.basic_authorize(name, password)
+    else
+      raise "I don't know how to log in!"
+    end
+  end
+
+  def remaining_days
+    meal_plan_end_date - Date.tomorrow
+  end
+
+  def meal_plan_end_date
+    MEAL_PLAN_END_DATE
+  end
+end
+
+EServices.new.call
